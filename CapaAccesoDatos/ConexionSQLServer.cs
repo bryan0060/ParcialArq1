63633cc717a592f078ac20330deff0bed56e5d1c
grandoghp <simonriveramunera@gmail.com>
2026-03-18 17:05:30 -0500

    Agregada la Capa de Acceso a Datos

---
+++ CapaAccesoDatos/ConexionSQLServer.cs
@@ -0,0 +1,41 @@
+using System;
+using CapaNegocio;
+using Microsoft.Data.SqlClient;
+
+namespace CapaAccesoDatos
+{
+    public class ConexionSQLServer : ICentralRiesgo
+    {
+        public SqlConnection conexion;
+
+        public ConexionSQLServer()
+        {
+            // Se usa la misma cadena de conexión del ejemplo del profesor,
+            // pero conectándonos a la nueva base de datos DbParcialArq
+            this.conexion = new SqlConnection(@"Data Source=DESKTOP-UG9NHAN\SQLEXPRESS;Persist Security Info=True;User ID=sa;Password=sa;TrustServerCertificate=True;Initial Catalog=DbParcialArq");
+        }
+
+        public int ConsultarPuntaje(string tipoDoc, string nroDoc)
+        {
+            int puntaje = 0;
+            
+            // Query simple de selección de datos al igual que en CapaAccesoDatos del Ejemplo del profesor
+            string select = string.Format("SELECT Puntaje FROM CentralRiesgo WHERE TipoDoc = '{0}' AND NroDoc = '{1}'", tipoDoc, nroDoc);
+
+            conexion.Open();
+
+            SqlCommand comando = new SqlCommand(select, conexion);
+            SqlDataReader reader = comando.ExecuteReader();
+
+            // Si encuentra un registro, leemos el puntaje
+            if (reader.Read())
+            {
+                puntaje = reader.GetInt32(0); // Columna 0 es Puntaje
+            }
+
+            conexion.Close();
+
+            return puntaje;
+        }
+    }
+}
